6776179ba8de05a89c7728a11df875048c2a6d7e
Akshya52 <aks575994@gmail.com>
2024-12-15 10:34:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SpellX1 from './Spell/Spell';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellX1 />
+    </div>
+  );
+}
+
+export default App;
